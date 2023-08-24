26d7315d1a6dbf2bbf92bc823413060e7c1de97e
Akanksha <akanksharma0829@gmail.com>
2023-08-24 17:58:09 +0530

    again commit

--- src/GenericComponents/Footer.js
+++ src/GenericComponents/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 
@@ -16,40 +15,34 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="FooterContainer">
-        <div className="brand Footer_Brand">
-          <Link to={"/"}>
-            <div id='heading'><span id="Head3">The</span>
-              <h1 id="Head4">Siren</h1>
-            </div>
-          </Link>
+      <div className="brand Footer_Brand">           <Link to={"/"}>            
+       <div id='heading'><span id="Head3">The</span>
+            <h1 id="Head4">Siren</h1>
+             </div>
+           </Link>
           <div>
             <img id='footimg' src="https://img.freepik.com/free-vector/portrait-programmer-working-with-pc_23-2148212818.jpg?w=740&t=st=1692263024~exp=1692263624~hmac=6d382a6c87ffe54d234fae1805ca62d0bd56f1615520419d9c7081bcbf51aa3c" alt="img here" height={200} width={200}></img>
           </div>
-
-
-        </div>
+</div>
         <div className="Footer_text">
-          <div className="Footer_Pages">
-            <h3 className="QuickLink">Quick Link </h3>
-            <hr className="hr1" style={{ width: "125px", height: "3px", background: "white", border: "none", outline: "none", borderRadius: "5px", position: "absolute", top: "50px" }} />
-            <ul className="menu1">
-              <li><Link to={"/"} >Home</Link></li>
-              <li><Link to={"/Bollywood"} >Bollywood</Link></li>
-              <li><Link to={"/HollyWood"} >HollyWood</Link></li>
-              <li><Link to={"/Technology"} >Technology</Link></li>
-              <li><Link to={"/Fitness"} >Fitness</Link></li>
-              <li><Link to={"/Food"} >Food</Link></li>
-            </ul>
-            {/* ,right:"420px" */}
-          </div>
-          <div className="FooterContact">
-            <h3 className="Contact"> Contact </h3>
-            <hr className="hr1" style={{ width: "90px", height: "3px", background: "white", border: "none", outline: "none", borderRadius: "5px", position: "absolute", top: "210px" }} />
+        <div className="Footer_Pages">
+          <h3 className="Quick">Quick Link </h3>
+          <ul>
+          <li><Link to={"/"} >Home</Link></li>
+            <li><Link to={"/Bollywood"} >Bollywood</Link></li>
+            <li><Link to={"/HollyWood"} >HollyWood</Link></li>
+            <li><Link to={"/Technology"} >Technology</Link></li>
+            <li><Link to={"/Fitness"} >Fitness</Link></li>
+            <li><Link to={"/Food"} >Food</Link></li>
+          </ul>
+        </div>
+        <div className="FooterContact">
+            <h3 className="contact"> Contact US </h3>
             <Link to={"https://www.facebook.com"} target="_blank"><div className="social_child">  <FontAwesomeIcon icon={faFacebook} /></div></Link>
-            <Link to={"https://www.instagram.com/akanksharma111/"} target="_blank" ><div className="social_child">  <FontAwesomeIcon icon={faInstagram} /></div></Link>
-            <Link to={"https://github.com/Akanksha0808-git"} target="_blank"><div className="social_child">  <FontAwesomeIcon icon={faGithub} /></div></Link>
-            <Link to={"https://www.linkedin.com/in/akanksha-sharma-0808ak"} target="_blank"><div className="social_child">   <FontAwesomeIcon icon={faLinkedin} /></div></Link>
-          </div>
+             <Link to={"https://www.instagram.com/akanksharma111/"} target="_blank" ><div className="social_child">  <FontAwesomeIcon icon={faInstagram} /></div></Link>
+             <Link to={"https://github.com/Akanksha0808-git"} target="_blank"><div className="social_child">  <FontAwesomeIcon icon={faGithub} /></div></Link>
+             <Link to={"https://www.linkedin.com/in/akanksha-sharma-0808ak"} target="_blank"><div className="social_child">   <FontAwesomeIcon icon={faLinkedin} /></div></Link>
+        </div>
         </div>
 
       </div>
--- src/GenericComponents/Homeupper.js
+++ src/GenericComponents/Homeupper.js
@@ -11,7 +11,7 @@ const Homeupper = () => {
             <h3>Title of Vertical Gallery</h3>
             <p>trvel/August 7 2023</p>
             </div>
-            <img src="https://content.api.news/v3/images/bin/3cf9bf4f8184eec397559f00f0a2c572?width=650" alt="not Found" height={"515px"} width={"100%"} />
+            <img className="barbie" src="https://content.api.news/v3/images/bin/3cf9bf4f8184eec397559f00f0a2c572?width=650" alt="not Found" height={"515px"} width={"100%"} />
         </Link>
         </div>
         
@@ -23,7 +23,7 @@ const Homeupper = () => {
                     <h3>Title of Vertical Gallery</h3>
                     <p>trvel/August 7 2023</p>
                 </div>
-                    <img src="https://ichef.bbci.co.uk/news/820/cpsprodpb/DAC3/production/_130630065_gettyimages-1267327926.jpg" alt="not found" height={"250px"} width={"100%"}/>
+                    <img className="telecom" src="https://ichef.bbci.co.uk/news/820/cpsprodpb/DAC3/production/_130630065_gettyimages-1267327926.jpg" alt="not found" height={"250px"} width={"100%"}/>
                 </Link>
             </div>
 
